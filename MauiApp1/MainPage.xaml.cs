e49b0cb8bbd8a7131e95615613a9a7341b6af610
oree <digitech.pakar@gmail.com>
2021-09-24 14:36:20 +0300

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            bwv.RootComponents[0].ComponentType = typeof(TodoLib.App);
+        }
+    }
+}
---
+++ TodoLib/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoLib
+{
+    public class ToDoItem
+    {
+        public string? Text { get; set; }
+        public bool Done { get; set; }
+
+    }
+}
